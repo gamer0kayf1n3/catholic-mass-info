d798af45abb7305270abdcfe131a120f1289f249
Steven <81130943+gamer0kayf1n3@users.noreply.github.com>
2024-07-14 13:10:04 +0800

    Create english-min.js

---
+++ english-min.js
@@ -0,0 +1,1 @@
+const today=new Date,dateTwoDigit=today.getDate().toString().padStart(2,"0"),monthTwoDigit=(today.getMonth()+1).toString().padStart(2,"0"),year=today.getFullYear()%100;BASE_URL=`https://bible.usccb.org/bible/readings/${monthTwoDigit}${dateTwoDigit}${year}.cfm`;
