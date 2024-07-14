e9beef7b808b1b65188b3012af81dba4472e7983
Steven <81130943+gamer0kayf1n3@users.noreply.github.com>
2024-07-14 13:40:37 +0800

    Update english.js

--- english.js
+++ english.js
@@ -4,3 +4,4 @@ const monthTwoDigit = (today.getMonth() + 1).toString().padStart(2, '0');
 const year = today.getFullYear() % 100;
 
 BASE_URL = `https://bible.usccb.org/bible/readings/${monthTwoDigit}${dateTwoDigit}${year}.cfm`
+location.href = BASE_URL;
